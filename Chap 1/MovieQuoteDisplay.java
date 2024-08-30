3de5be66cbce48c1d3e8633cb9bb2aaba0f94810
Vrezyl Giduquio <vgiduquio@gmail.com>
2024-08-30 08:01:54 +0700

    Update MovieQuoteDisplay.java

--- Chap 1/MovieQuoteDisplay.java
+++ Chap 1/MovieQuoteDisplay.java
@@ -1,12 +1,11 @@
 public class MovieQuoteDisplay {
     public static void main(String[] args) {
-        // Variables to store the quote details
+        
         String quote = "Near Far";
         String movie = "Titanic";
         String character = "Jack and Rose";
         int year = 1977;
 
-        // Display the information
         System.out.println("Quote: \"" + quote + "\"");
         System.out.println("Movie: " + movie);
         System.out.println("Character: " + character);
