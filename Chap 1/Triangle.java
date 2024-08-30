b8fba23bed71907d859e984bf0e9af2c4e1e4e3e
Vrezyl Giduquio <vgiduquio@gmail.com>
2024-08-30 07:59:07 +0700

    Update Triangle.java

--- Chap 1/Triangle.java
+++ Chap 1/Triangle.java
@@ -1,17 +1,17 @@
 public class Triangle{
     public static void main(String[] args) {
-        int rows = 5;  // Number of rows in the triangle
+        int rows = 5; 
 
         for (int i = 1; i <= rows; i++) {
-            // Print spaces to align the 'T's to the right
+            
             for (int j = i; j < rows; j++) {
                 System.out.print(" ");
             }
-            // Print 'T' characters to form the triangle
+           
             for (int k = 1; k <= (2 * i - 1); k++) {
                 System.out.print("T");
             }
-            // Move to the next line after each row
+           
             System.out.println();
         }
     }
