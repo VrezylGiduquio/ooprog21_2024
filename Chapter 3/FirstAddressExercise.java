0b87cd89bba0e72dd128e2c72597c42173f9d1a3
Vrezyl Giduquio <vgiduquio@gmail.com>
2024-09-20 21:59:46 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,14 @@
+public class FirstAddressExercise {
+    public static void main(String[] args) {
+        String brand = "BlackBerry";
+        String address = "Waterloo,200 University Ave, Canada";
+        String name = "Company:";
+        
+        
+        String company = "BlackBerry";
+
+        System.out.println("Brand: " + brand);
+        System.out.println("Address: " + address);
+        System.out.println("Company: " + company);
+    }
+}
